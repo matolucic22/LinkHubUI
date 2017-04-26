6a3dd054a742f52d87b1eeeb9df2f79554258a54
Mato Lučić <mlucic@etfos.hr>
2017-04-26 12:54:25 +0200

    Add controllers and index views

---
+++ LinkHubUI/LinkHubUI/Areas/User/Controllers/URLController.cs
@@ -0,0 +1,26 @@
+﻿using BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkHubUI.Areas.User.Controllers
+{
+    public class URLController : Controller
+    {
+        // GET: User/URL
+        public ActionResult Index()//display form
+        {
+            LinkHubDbEntities db = new LinkHubDbEntities();
+            ViewBag.CategoryId = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");//table, selected value-value field, displaied value-text field
+            return View();
+        }
+        public ActionResult Create(tbl_Url objUrl)
+        {
+            //LinkHubDbEntities db = new LinkHubDbEntities();
+            //ViewBag.CategoryId = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");//table, selected value-value field, displaied value-text field
+            return View();
+        }
+    }
+}
